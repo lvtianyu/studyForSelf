e904300c0c293ab2263845b525306c7b4367555b
lvtianyu <lv1464562713@163.com>
2017-04-12 13:36:26 +0800

    ce

---
+++ dist/canvas.js
@@ -0,0 +1,81 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "http://10.0.1.18:2017/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	var target = document.getElementById('canvas');
+	
+	target.onmouseenter = function (ev) {
+	    startX = ev.clientX;
+	};
+	target.onmouseleave = function (ev) {};
+	target.onmousemove = function (ev) {
+	    var client = ev.clientX; //pc版
+	};
+	
+	var canvas = document.createElement("canvas");
+	document.body.appendChild(canvas);
+	var ctx = canvas.getContext('2d');
+	
+	ctx.beginPath();
+	ctx.fillStyle = "green";
+	ctx.fillRect(0, 0, 500, 500);
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.fillStyle = 'blue';
+	ctx.fillRect(0, 0, 50, 50);
+	ctx.closePath();
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=canvas.js.map
